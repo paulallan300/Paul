c154c3298eec0af9bb5c730edff1857db1bf3f50
Paul Allan <65020951+paulallan300@users.noreply.github.com>
2022-01-23 20:31:47 +0530

    Update Mini.java

--- Mini.java
+++ Mini.java
@@ -1,5 +1,7 @@
 
-Mini Project on Hospital Management System.
+Mini Project on Hospital Management System and Online Quiz System.
 
 This is a java program which is used to update the hospital management like 
 the availability or name of doctors, patients, medicines available etc.
+  
+For java program on online quiz it helps students to attend tests at their comfort of their zone.  
